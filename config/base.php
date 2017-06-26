fa719cbfe63d788463cfc1c92d0d1fe1159e188d
Simeon <lyubenov@gmail.com>
2017-06-26 14:50:45 +0300

    update

--- config/base.php
+++ config/base.php
@@ -1,5 +1,5 @@
 <?php
 /* BASE SETTINGS */
-define('BASE_PATH',"/phreak"); // leave empty if not in subdir or "/phreak" for example subdir
+define('BASE_PATH',""); // leave empty if not in subdir or "/phreak" for example subdir
 define('BASE_URL',"http://".$_SERVER['HTTP_HOST'].BASE_PATH);
 define('ROOT_DIR', dirname(__DIR__));
