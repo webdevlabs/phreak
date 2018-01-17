ee7bcde314ba1a8856202b6592bb58431945e2a8
webdevlabs <office@webdevlabs.com>
2018-01-17 11:26:44 +0200

    base config update

--- config/base.php
+++ config/base.php
@@ -1,5 +1,5 @@
 <?php
 /* BASE SETTINGS */
-define('BASE_PATH',"/phreak"); // leave empty if not in subdir or "/phreak" for example subdir
+define('BASE_PATH',""); // if running in subdir, leave empty if not or running with php's built-in webserver
 define('BASE_URL',"http://".$_SERVER['HTTP_HOST'].BASE_PATH);
 define('ROOT_DIR', dirname(__DIR__));
