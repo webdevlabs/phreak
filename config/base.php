63d8e16ccfa50bd371dd9780f25be13760a1ceab
webdevlabs <office@webdevlabs.com>
2018-01-24 21:31:28 +0200

    added StyleCI

--- config/base.php
+++ config/base.php
@@ -1,6 +1,6 @@
 <?php
 
 /* BASE SETTINGS */
-define('BASE_PATH', ''); // if running in subdir, leave empty if not or running with php's built-in webserver
+define('BASE_PATH', '/phreak'); // if running in subdir, leave empty if not or running with php's built-in webserver
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].BASE_PATH);
 define('ROOT_DIR', dirname(__DIR__));
