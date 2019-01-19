2b249f745d82f2532c9958fff8396da1a7adf913
Web Development Labs <office@webdevlabs.com>
2019-01-19 10:42:27 +0000

    Apply fixes from StyleCI

--- app/commands/clearPushSubscriptions.php
+++ app/commands/clearPushSubscriptions.php
@@ -1,15 +1,14 @@
 <?php
 
 namespace App\Commands;
+
 use System\DB;
 
 class clearPushSubscriptions
 {
-
-    public function __construct () 
+    public function __construct()
     {
-        $results = DB::column("SELECT COUNT(*) from `push_subscriptions` WHERE `added` < (NOW() - INTERVAL 1 DAY)");
+        $results = DB::column('SELECT COUNT(*) from `push_subscriptions` WHERE `added` < (NOW() - INTERVAL 1 DAY)');
         echo 'Deleted '.$results.' records from database';
     }
-
 }
